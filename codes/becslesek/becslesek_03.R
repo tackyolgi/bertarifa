00e7c7c9ec2796c51ecb93d577b21d671ed4fe0e
tackyolgi <tackyolgi@gmail.com>
2019-08-10 15:01:08 +0200

    elkeszult a fuggvenyeket tartalmazo becslesek file

---
+++ codes/becslesek/becslesek_03.R
@@ -0,0 +1,108 @@
+# this script contains the A, B and C estimations 
+#    what Vincze mentioned in his e-mail on 5th of Aug
+# it contains oaxaca and rf estimations only
+
+# „A” változat: csak a fenti változók szerepeljenek (a regresszióban a "kor" négyzet is)
+
+# „B” változat: a fenti változók + feor (2 számjegyű)
+
+# „C” változat: a fenti változók + nők aránya
+
+
+# R1: szerepel a „nem” a magyarázó változók között
+
+# R2: nem szerepel a „nem” a magyarázó változók között
+
+# sampling
+get_sample <- function(df, obs_num){
+  index <- sample(seq_len(nrow(df)), obs_num)
+  train <- df[index, ]
+  test <- df[-index, ]
+  list(train=train, test=test)
+}
+
+# oaxaca
+get_A_oax_regr <- function(df) {
+  oax_decomp <- oaxaca(formula = lnker~  iskveg9 +ujbel + kor +kor2+szolgho+letszam_bv1
+                       + ag1+ ksz+ kag+kol+ ara+ kra+   kshreg + ttip+szesu_v1 - 1
+                       | nem
+                       , data = df,
+                       # Neumark does not include group indicator variable
+                       R = 10)
+}
+
+get_B_oax_regr <- function(df) {
+  oax_decomp <- oaxaca(formula = lnker~  iskveg9 +ujbel + kor +kor2+szolgho+letszam_bv1
+                       + ag1+ ksz+ kag+kol+ ara+ kra+   kshreg + ttip+szesu_v1+ feor_2 - 1
+                       | nem
+                       , data = df,
+                       # Neumark does not include group indicator variable
+                       R = 10)
+}
+
+get_C_oax_regr <- function(df) {
+  oax_decomp <- oaxaca(formula = lnker~  iskveg9 +ujbel + kor +kor2+szolgho+letszam_bv1
+                       + ag1+ ksz+ kag+kol+ ara+ kra+   kshreg + ttip+szesu_v1+ nok_aranya - 1
+                       | nem
+                       , data = df,
+                       # Neumark does not include group indicator variable
+                       R = 10)
+}
+
+
+# random forest
+get_A_oax_RF_R1 <- function(df, valt_szama, level_szam, num_tree, feor) {
+    randfor_ref <- randomForest(lnker ~ nem+iskveg9 +ujbel + kor +kor2+szolgho+letszam_bv1
+                                + ag1+ ksz+ kag+kol+ ara+ kra+   kshreg + ttip+szesu_v1,
+                                na.action=na.omit, mtry=valt_szama,
+                                nodesize=level_szam, ntree=num_tree,
+                                importance = F,proximity=F,
+                                data=df)
+}
+
+get_A_oax_RF_R2 <- function(df, valt_szama, level_szam, num_tree, feor) {
+  randfor_ref <- randomForest(lnker ~ iskveg9 +ujbel + kor +kor2+szolgho+letszam_bv1
+                              + ag1+ ksz+ kag+kol+ ara+ kra+   kshreg + ttip+szesu_v1,
+                              na.action=na.omit, mtry=valt_szama,
+                              nodesize=level_szam, ntree=num_tree,
+                              importance = F,proximity=F,
+                              data=df)
+}
+
+get_B_oax_RF_R1 <- function(df, valt_szama, level_szam, num_tree, feor) {
+  randfor_ref <- randomForest(lnker ~ nem+iskveg9 +ujbel + kor +kor2+szolgho+letszam_bv1
+                              + ag1+ ksz+ kag+kol+ ara+ kra+   kshreg + ttip+szesu_v1+ feor_2,
+                              na.action=na.omit, mtry=valt_szama,
+                              nodesize=level_szam, ntree=num_tree,
+                              importance = F,proximity=F,
+                              data=df)
+}
+
+get_B_oax_RF_R2 <- function(df, valt_szama, level_szam, num_tree, feor) {
+  randfor_ref <- randomForest(lnker ~ iskveg9 +ujbel + kor +kor2+szolgho+letszam_bv1
+                              + ag1+ ksz+ kag+kol+ ara+ kra+   kshreg + ttip+szesu_v1+ feor_2,
+                              na.action=na.omit, mtry=valt_szama,
+                              nodesize=level_szam, ntree=num_tree,
+                              importance = F,proximity=F,
+                              data=df)
+}
+
+get_c_oax_RF_R1 <- function(df, valt_szama, level_szam, num_tree, feor) {
+  randfor_ref <- randomForest(lnker ~ nem+iskveg9 +ujbel + kor +kor2+szolgho+letszam_bv1
+                              + ag1+ ksz+ kag+kol+ ara+ kra+   kshreg + ttip+szesu_v1
+                              + nok_aranya,
+                              na.action=na.omit, mtry=valt_szama,
+                              nodesize=level_szam, ntree=num_tree,
+                              importance = F,proximity=F,
+                              data=df)
+}
+
+get_C_oax_RF_R2 <- function(df, valt_szama, level_szam, num_tree, feor) {
+  randfor_ref <- randomForest(lnker ~ iskveg9 +ujbel + kor +kor2+szolgho+letszam_bv1
+                              + ag1+ ksz+ kag+kol+ ara+ kra+   kshreg + ttip+szesu_v1
+                              + nok_aranya,
+                              na.action=na.omit, mtry=valt_szama,
+                              nodesize=level_szam, ntree=num_tree,
+                              importance = F,proximity=F,
+                              data=df)
+}
